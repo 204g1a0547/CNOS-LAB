1aa3795a0703b31426b3ee0784b6287326a9af40
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-22 12:10:17 +0530

    lalitha created pro.c

---
+++ OSLAB/pro.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: R.Lalitha
+   Roll No: 204G1A0501
+   Title of the Experiment: To simulate producer-consumer problem using semaphores.
+   Date of Creation: 22-11-2022
+   Date of Execution: 22-11-2022
+*/
