15905318c43b0bbcd8350db23389ecac6c8c0953
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-26 14:55:58 +0530

    lalitha created FCFS.c

---
+++ OSLAB/FCFS.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: R.Lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 26-11-2022
+   Date of Execution: 26-11-2022
+*/
