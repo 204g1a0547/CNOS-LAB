f1d494e81c5763313444dd159a507e96276ee787
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-26 15:15:41 +0530

    lalitha created SRJ.c

---
+++ OSLAB/SJF.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 26-11-2022
+   Date of Execution: 26-11-2022
+*/
