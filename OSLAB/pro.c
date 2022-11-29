b038b8d0d720024301060f81cf2de2041303eb97
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-29 12:31:12 +0530

    lalitha created pro.c

---
+++ OSLAB/pro.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-11-2022
+   Date of Execution: 29-11-2022
+*/
