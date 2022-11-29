9256acec48c1bfacd7407b8c84d282751c508f57
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-29 12:05:18 +0530

    lalitha created pri.c

---
+++ OSLAB/pri.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: lalitha
+   Roll No: 204G1A0547
+   Title of the Experiment: cpu scheduling using priority
+   Date of Creation: 29-11-2022
+   Date of Execution: 29-11-2022
+*/
