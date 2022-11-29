a2aa2e074d1714a5745525548faf47daed57c703
204g1a0547 <94514824+204g1a0547@users.noreply.github.com>
2022-11-29 12:36:02 +0530

    lalitha created mvt.c

---
+++ OSLAB/mvt.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate:laltha
+   Roll No: 204G1A0547
+   Title of the Experiment: to stimulate program by MVT
+   Date of Creation: 29-11-2022
+   Date of Execution: 29-11-2022
+*/
